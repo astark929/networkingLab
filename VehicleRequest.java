436a645a45b2c9f70da5fb53b8c670b7789e9e83
Aaron <33184112+astark929@users.noreply.github.com>
2022-10-16 22:42:15 -0700

    Update VehicleRequest.java

--- VehicleRequest.java
+++ VehicleRequest.java
@@ -14,28 +14,4 @@ public class VehicleRequest implements Serializable{
 
         message = "You requested a " + year + " " + make + " " + model + ".";
     }
-
-    public int getYear(){
-        return year;
-    }
-
-    public String getMake(){
-        return make;
-    }
-
-    public String getModel(){
-        return model;
-    }
-
-    public void setYear(int year){
-        this.year = year;
-    }
-
-    public void setMake(String make){
-        this.make = make;
-    }
-
-    public void setModel(String model){
-        this.model = model;
-    }
 }
